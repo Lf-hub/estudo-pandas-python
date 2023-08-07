43d708dafacc15d33b34fd5aee289ffcd55898f6
Luis Fernando Giglio <luisfgiglio@gmail.com>
2023-08-06 23:16:25 -0300

    [WIP] Validações

--- common/views.py
+++ common/views.py
@@ -1,8 +1,11 @@
+import pandas as pd
+
 from django.views import View
 from django.shortcuts import render
 
 from common.models import File
-from common.forms import ImportFileForm
+from common.parameters import DEFAULT_LIST
+
 
 
 class CommonIndexView(View):
@@ -14,20 +17,44 @@ class ImportFile(View):
     model = File
     
     def post(self, request):
-        # form = ImportFileForm
-        import pandas as pd
+        def contar_pares_impares(row):
+            pares = sum(1 for num in row if num % 2 == 0)
+            impares = len(row) - pares
+            return pares, impares
+        
+        def contem_mestre(row):
+            for value in row:
+                if value in DEFAULT_LIST.get('mestres'):
+                    return 1
+            return 0
+        
+        def contem_mestre(row):
+            for value in row:
+                if value in DEFAULT_LIST.get('mestres'):
+                    return 1
+            return 0
+
         file = self.request.FILES.get('file')
-
-        # Dentro do process
         dataframe = pd.read_excel(file)
-        dt = dataframe
-
-        def contar_pares_impares(numero):
-            if numero % 2 == 0:
-                return 'Par'
-            else:
-                return 'Ímpar'
-
-
-
+        dataframe[['pares', 'impares']] = dataframe.apply(contar_pares_impares, axis=1, result_type='expand')
+        dataframe['mestre'] = dataframe.apply(contem_mestre, axis=1)
         return render(request, 'index.html')
+
+    
+    
+
+
+    # def parans(self):
+    #     "upper_quadrant":[1 a 50],
+    #     "lower_quadrant":[51 a 100],
+    
+    #     "first_line": [1 a 10],
+    #     "second_line": [11 a 20],
+    #     "third_line": [21 a 30],
+    #     "fourth_line": [31 a 40],
+    #     "fifth_line": [41 a 50],
+    #     "sixth_line": [51 a 60],
+    #     "seventh_line": [61 a 70],
+    #     "octave_line": [71 a 80],
+    #     "ninth_line": [81 a 90],
+    #     "tenth_line": [91 a 100],
